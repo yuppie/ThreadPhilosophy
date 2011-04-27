70a6cc7d8bd8450c9d139ed3e3a591589be1c2d2
yuppie <yuppiehoyl@gmail.com>
2011-04-27 12:47:33 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: dima
+ *
+ * Created on 27 Апрель 2011 г., 12:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
